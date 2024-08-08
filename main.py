fd2a8dbe479956bb51f310d1794693eeeaca5732
love4m <shinibingo@gmail.com>
2024-08-08 17:11:20 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import electric_car as ec
+import pybasic as pb
+
+
+#로그인 화면
+st.sidebar.title("로그인")
+user_id = st.sidebar.text_input("아이디(ID)입력",value="",max_chars=10)
+user_pw = st.sidebar.text_input("패스워드 입력",value="",type='password')
+
+
+if user_id == "abc" and user_pw == "1234":
+    st.sidebar.title("★ 승신 포트폴리오 ★")
+    #st.image('mo_p.jpg')
+
+
+    menu = st.sidebar.radio('메뉴선택',['파이썬 기초','탐색적 분석 : 전기자동차','머신러닝'],index=None)
+    st.sidebar.write(menu)
+
+
+    if menu == '탐색적 분석 : 전기자동차':
+        ec.elec_exe()
+    elif menu == '머신러닝':
+        st.header('공사중')
+    elif menu == '파이썬 기초':
+        pb.basic()
+
---
+++ pybasic.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import random
+
+def gugudan():
+    dan = st.number_input("단입력>>",value=1)
+
+    if dan>1:
+        for i in range(1,10):
+        #gop5=a*i
+            st.write(f"{dan}*{i}={dan*i}")
+
+
+def recommand_food():
+#음식추천프로그램
+
+    c_food=['짜장면','짬뽕','팔보채']
+    k_food=['갈비탕','육개장','된장찌개','비빔밥','김치찌개']
+
+    menu=st.radio("음식추천",["중식","한식"],index=None)
+    if menu=="중식":
+        st.write(f"오늘의 중식 추천 메뉴: {random.choice(c_food)}")
+    elif menu=="한식":
+        st.write(f"오늘의 한식 추천 메뉴: {random.choice(k_food)}")
+    else:
+        st.write("음식종류를 선택하세요")
+
+
+
+def basic():
+    tab1, tab2 = st.tabs(["구구단", "음식추천"])
+
+    with tab1:
+        gugudan()
+    with tab2:
+        recommand_food()
